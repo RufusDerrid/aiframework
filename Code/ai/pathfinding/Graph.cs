35e572af1ab440ebc3dd9b18ec66dab27d813653
rufusderrid <rufusderrid@gmail.com>
2018-02-18 20:12:20 +0300

    Dijkstra

--- Code/ai/pathfinding/Graph.cs
+++ Code/ai/pathfinding/Graph.cs
@@ -64,6 +64,132 @@ namespace Assets.Code.ai.pathfinding
             return _connections[fromNode.GetId()];
         }
 
+        public List<Connection> PathfindDijkstra(Node start, Node end)
+        {
+            var startRecord = new NodeRecord();
+            startRecord.Node = start;
+            startRecord.Connection = null;
+            startRecord.CostSoFar = 0;
+
+            var open = new List<NodeRecord>();
+            open.Add(startRecord);
+            var closed = new List<NodeRecord>();
+            NodeRecord current = null;
+
+            while (open.Count > 0)
+            {
+                current = SmallestElement(open);
+                if(current.Node == end)
+                {
+                    break;
+                }
+
+                var connections = GetConnections(current.Node);
+                foreach(var connection in connections)
+                {
+                    var toNode = connection.GetToNode();
+                    var toNodeCost = current.CostSoFar + connection.GetCost();
+                    NodeRecord toNodeRecord = null;
+
+                    if(NodeRecordsContains(closed, toNode))
+                    {
+                        continue;
+                    }
+                    else if(NodeRecordsContains(open, toNode))
+                    {
+                        toNodeRecord = FindByNode(open, toNode);
+                        if (toNodeRecord.CostSoFar <= toNodeCost)
+                        {
+                            continue;
+                        }
+                    }
+                    else
+                    {
+                        toNodeRecord = new NodeRecord();
+                        toNodeRecord.Node = toNode;
+                    }
+
+                    toNodeRecord.CostSoFar = toNodeCost;
+                    toNodeRecord.Connection = connection;
+
+                    if (!NodeRecordsContains(open, toNode))
+                    {
+                        open.Add(toNodeRecord);
+                    }
+                }
+
+                open.Remove(current);
+                closed.Add(current);
+            }
+
+            if (current.Node != end)
+            {
+                return null;
+            }
+            else
+            {
+                var path = new List<Connection>();
+                while(current.Node != start)
+                {
+                    path.Add(current.Connection);
+                    current = FindByNode(closed, current.Connection.GetFromNode());
+                    path.Reverse();
+
+                    return path;
+                }
+            }
+        }
+
+        private NodeRecord FindByNode(List<NodeRecord> records, Node node)
+        {
+            foreach (var record in records)
+            {
+                if (record.Node == node)
+                {
+                    return record;
+                }
+            }
+
+            return null;
+        }
+
+        private bool NodeRecordsContains(List<NodeRecord> records, Node node)
+        {
+            foreach(var record in records)
+            {
+                if (record.Node == node)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private NodeRecord SmallestElement(List<NodeRecord> records)
+        {
+            if (records.Count > 0)
+            {
+                float smallestCost = records[0].CostSoFar;
+                int smallestIndex = 0;
+
+                for (int i = 1; i < records.Count; i++)
+                {
+                    if(records[i].CostSoFar < smallestCost)
+                    {
+                        smallestCost = records[i].CostSoFar;
+                        smallestIndex = i;
+                    }
+                }
+
+                return records[smallestIndex];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
         private List<Connection> CreateConnections(Node node, List<Node> nodes, float cost)
         {
             var leftPosition = node.GetPosition() + new Vector2(-1, 0);
---
+++ Code/ai/pathfinding/NodeRecord.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Code.ai.pathfinding
+{
+    public class NodeRecord
+    {
+        public Node Node;
+        public Connection Connection;
+        public float CostSoFar;
+    }
+}
